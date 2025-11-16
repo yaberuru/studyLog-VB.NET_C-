8489caa1dbdfe48e0da72d00fd6113adb8e19bff
yaberuru <sachi-0214-star@ezweb.ne.jp>
2025-11-16 21:04:11 +0900

    Memoクラスの新規作成

---
+++ backend/CSharpLib/model/Memo.cs
@@ -0,0 +1,10 @@
+namespace CSharpLib.Models 
+{
+    public class Memo
+    {
+        public int Id { get; set; }
+        public string Text { get; set; } = "";
+        public string Context { get; set;} = "";
+    }
+
+}
